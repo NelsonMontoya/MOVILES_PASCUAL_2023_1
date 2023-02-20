fcbf93505a1cb55696d6d75fdc584e821e5e65f4
maksilorak <maksilorak@gmail.com>
2023-02-20 15:01:54 -0500

    Creación Proyecto
    Proyecto base para realizar en la clase del 20 de febrero de 2023, para la asignatura Programación de Dispositivos Móviles, Institución Pascual Bravo

---
+++ Clase20febrero/app/src/main/java/com/movilespascual/clase20febero2023/MainActivity.java
@@ -0,0 +1,20 @@
+package com.movilespascual.clase20febero2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    // Aqui se ponen las variables que se van a usar globalmente
+    String TAG = "ActividadUno";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG,"--OnCreate--"); //Para depuración
+    }
+
+    
+}
